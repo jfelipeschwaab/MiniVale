2d7cf9da4b4a14ed4b1d25925ba60290c7da18e8
jfelipeschwaab <joaofsp.games@gmail.com>
2026-06-15 17:40:22 -0300

    :construction: Criado o Repository que acessará a idempotency key Entity

--- src/main/java/com/example/demo/repository/IdempotencyKeyRepository.java
+++ src/main/java/com/example/demo/repository/IdempotencyKeyRepository.java
@@ -1,4 +1,12 @@
 package com.example.demo.repository;
 
-public interface IdempotencyKeyRepository {
+import com.example.demo.entity.IdempotencyKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IdempotencyKeyRepository extends JpaRepository<IdempotencyKey, Long> {
+
+    Optional<IdempotencyKey> findByChave(String chave);
 }
+
